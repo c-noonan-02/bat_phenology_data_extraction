191161f84eefe48ba65f343d93ab86c8c8a915c0
c-noonan-02 <charlottenoonan@outlook.com>
2025-03-26 22:44:51 +0000

    Set-up to extract data from figures taken from the papers identified in the meta-analysis.

---
+++ activity_levels/activity_data_extraction.R
@@ -0,0 +1,60 @@
+# clear environment
+rm(list = ls())
+
+# import required packages
+library(dplyr)
+library(shinyDigitise)
+
+#### Shiny Digitise ####
+
+# code for shiny digitise
+figures_data <- shinyDigitise(dir = normalizePath("./activity_levels/figures"))
+
+#### Data from Lou et al. (2012) ####
+
+# a
+
+
+#### Data from Hooker et al. (2012) ####
+
+# a
+
+
+#### Data from Schamhart et al. (2024) ####
+
+# a
+
+
+#### Data from Seewagen et al. (2021) ####
+
+# a
+
+
+#### Data from Zeale et al. (2018) ####
+
+# a
+
+
+#### Data from Russo et al. (2019) ####
+
+# a
+
+
+#### Data from Froidevaux et al. (2018) ####
+
+# a
+
+
+#### Data from Haddock et al. (2019) ####
+
+# a
+
+
+#### Data from Rowse et al. (2018) ####
+
+# a
+
+
+#### Data from Stone et al. (2009) ####
+
+# a
